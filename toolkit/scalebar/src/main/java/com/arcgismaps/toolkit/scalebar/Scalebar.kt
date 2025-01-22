e78b92d2cebc181bc5ecee60ac4b3d4cc5089a55
Puneet Prakash <puneet_prakash@esri.com>
2025-01-22 13:29:39 -0800

    WIP

--- toolkit/scalebar/src/main/java/com/arcgismaps/toolkit/scalebar/Scalebar.kt
+++ toolkit/scalebar/src/main/java/com/arcgismaps/toolkit/scalebar/Scalebar.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.toolkit.scalebar.internal.GraduatedLineScalebar
 import com.arcgismaps.toolkit.scalebar.internal.LineScalebar
 import com.arcgismaps.toolkit.scalebar.internal.ScalebarDivision
 import com.arcgismaps.toolkit.scalebar.internal.ScalebarUtils.toPx
@@ -135,7 +136,12 @@ private fun ShowScalebar(
         ScalebarStyle.AlternatingBar -> TODO()
         ScalebarStyle.Bar -> TODO()
         ScalebarStyle.DualUnitLine -> TODO()
-        ScalebarStyle.GraduatedLine -> TODO()
+        ScalebarStyle.GraduatedLine -> GraduatedLineScalebar(
+            modifier = modifier,
+            maxWidth = maxWidth.toFloat(),
+            tickMarks = labels,
+            colorScheme = colorScheme,
+        )
         ScalebarStyle.Line -> LineScalebar(
             modifier = modifier,
             maxWidth = maxWidth.toFloat(),
